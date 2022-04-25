af85c90d1054c9f2527dca2945fd078cc6311a9b
sararoseane <sararoseanesm@gmail.com>
2022-04-25 13:37:01 -0300

    imagens

---
+++ os/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  	<title>Catedral Automação - Sistema de Automação Comercial</title>
+  	
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  	<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+  	<link href="css/animate.css" rel="stylesheet">
+  	<link href="css/style.css" rel="stylesheet">
+    <link href="css/catedral.css" rel="stylesheet">
+    
+</head>
+
+<body class="gray-bg">
+	
+    <div class="loginColumns animated fadeInDown">
+		<div class="row">
+			<div class="col-md-8">
+           		<img src="img/logoIndex.png" class="logoIndex" />
+        	</div>
+        	<div class="col-md-4">
+				<div class="inqbox-content">
+                	<p class="text-center">
+						Ordem de Serviço
+              		</p>
+					<form class="m-t" role="form" action="functions/ct.php" method="post">
+						<div class="form-group">
+                    		<input type="text" class="form-control" name="login" placeholder="Login" required>
+                 		</div>
+                 		<div class="form-group">
+                    		<input type="password" class="form-control" name="senha" placeholder="Senha" required>
+                 		</div>
+                 		<button type="submit" class="btn btn-primary block full-width m-b">Entrar</button>
+              		</form>
+				</div>
+			</div>
+		</div>
+     
+		<hr/>
+     
+		<div class="row">
+			<div class="col-md-8"> 
+        	</div>
+        	<div class="col-md-4 text-center">
+				<small>Copyright Catedral Automação © 2022</small>
+        	</div>
+     	</div>
+     
+	</div>
+    
+</body>
+</html>
